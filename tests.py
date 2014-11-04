8df8595d0dec428e593b4089496e835034872ee3
Gareth <gmackrill@mercycorps.org>
2014-11-03 20:07:44 -0800

    Adds phrase-list tests

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ import phrase
 import parse
 import config
 
-COUNTRIES = phrase.PhraseListFileFactory.factory(config.COUNTRYLIST_SOURCE['connection_string'])
+COUNTRIES = phrase.PhraseListFileFactory.factory(connection_string=config.COUNTRYLIST_SOURCE.connection_string, parameters=config.COUNTRYLIST_SOURCE.parameters)
 
 email_stub = {'subject': 'Donate now!',
               'html':'are we talking about niger? or canada or somewhere else?. No, niger.'
@@ -14,6 +14,58 @@ email_stub = {'subject': 'Donate now!',
               , 'country_parser':  parse.PopularityParser(possible_phrases=COUNTRIES)}
 stub_metadata = {'country': 'niger', 'signer': 'John Doe'}
 
+
+class TestPhraseListFileFactory(unittest.TestCase):
+    def setUp(self):
+        self.factory = phrase.PhraseListFileFactory
+        self.expected_phrase_list = phrase.PhraseList(['canada', 'united states', 'new zealand'])
+
+    def runTest(self):
+        self.assertListEqual(self.factory.factory('./test_files./phrase_list.csv',{'line_separator': '\n'}), self.expected_phrase_list)
+
+
+class TestPhraseListConfiguration(unittest.TestCase):
+    def setUp(self):
+        self.expected_configuration_parameters = {'line_separator': '\n'}
+        self.configuration = config.PhraseListConfiguration(None, None, line_separator='\n')
+    def runTest(self):
+        self.assertDictEqual(self.expected_configuration_parameters, self.configuration.parameters)
+
+
+class TestIdentificationOfPhrases(unittest.TestCase):
+    def setUp(self):
+        self.possible_phrases = phrase.PhraseList(['china', 'france', 'germany', 'ireland', 'england', 'mali'])
+        self.expected_phrases_to_find = ['england', 'france']
+        self.parser = parse.PhraseParser('The countries of England, Somalia, and Francis -- er, I mean france.', self.possible_phrases)
+
+    @unittest.expectedFailure
+    def runTest(self):
+        self.assertListEqual(sorted(self.expected_phrases_to_find),
+                             sorted(self.parser.phrases_in_parseable()))
+
+
+class TestPopularityCounter(unittest.TestCase):
+    def setUp(self):
+        self.possible_phrases = phrase.PhraseList(['england'])
+        self.parser = parse.PopularityParser('The countries of England, Somalia, and Francis -- er, I mean france.', self.possible_phrases)
+        self.instances_of_phrase = self.parser.count_instances(self.possible_phrases[0])
+        self.expected_instances_of_phrase = 1
+
+    def runTest(self):
+        self.assertEqual(self.expected_instances_of_phrase, self.instances_of_phrase)
+
+
+class TestEmailInstantiationUsingDefaults(unittest.TestCase):
+    def setUp(self):
+        self.encountered_error = False
+        try:
+            Email(**email_stub)
+        except:
+            self.encountered_error = True
+
+    def runTest(self):
+        self.assertFalse(self.encountered_error)
+        
 class TestSubject(unittest.TestCase):
     def setUp(self):
         self.email = Email(**email_stub)
@@ -28,7 +80,7 @@ class TestCountry(unittest.TestCase):
 
     def runTest(self):
         self.assertEqual(self.email.country, stub_metadata['country'])
-        
+
 
 class TestSigner(unittest.TestCase):
     def setUp(self):
