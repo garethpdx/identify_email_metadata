39e97eccea1cf1c970ba72f4deb5785f691bb926
Gareth <gmackrill@mercycorps.org>
2014-12-06 13:42:29 -0800

    Renames phraselist

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ Edit this script to configure new phrase data sources.
 from configuration import PhraseListConfiguration
 
 COUNTRYLIST_SOURCE = PhraseListConfiguration(method='file',
-                                             connection_string='./mc_countries.csv',
+                                             connection_string='./phraselist.csv',
                                              line_separator = '\n')
